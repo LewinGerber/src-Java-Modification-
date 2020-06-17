2ff4b7e7f548fcee97c0cb2ea554931ced3b4cf3
saexymama <66903629+saexymama@users.noreply.github.com>
2020-06-17 23:56:47 +0200

    adventure time sword added

--- main/java/com/example/nawm/init/ItemInit.java
+++ main/java/com/example/nawm/init/ItemInit.java
@@ -2,8 +2,10 @@ package com.example.nawm.init;
 
 import com.example.nawm.AnimeWeaponsMod;
 import com.example.nawm.objects.items.BasicItem;
+import com.example.nawm.objects.items.ToolTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,14 +21,29 @@ import net.minecraftforge.registries.ObjectHolder;
 @ObjectHolder(AnimeWeaponsMod.MOD_ID)
 public class ItemInit {
 
+    public static final Item basic_item = null;
+    public static final Item adventure_sword = null;
+
     @SubscribeEvent
     public static void registerItems(final RegistryEvent.Register<Item> event) {
+
         //items
         event.getRegistry().register(new BasicItem().setRegistryName("basic_item"));
 
         //blocks
+
+
         //block items
+
+
         //armor
+
+
         //tools
+        event.getRegistry().register(new SwordItem(ToolTier.AdventureTier.ADVENTURE, 7, 5.0F, new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName("adventure_sword"));
+
+        //food
+
+
     }
 }
--- main/java/com/example/nawm/objects/items/ToolTier.java
+++ main/java/com/example/nawm/objects/items/ToolTier.java
@@ -1,9 +1,68 @@
 package com.example.nawm.objects.items;
 
+import com.example.nawm.init.ItemInit;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
 /**
- * @author Lewin Gerber
- * @version 16.06.2020
+ * @author Elias Mehran
+ * @version 17.06.2020
  * nAWM
  */
 public class ToolTier {
+
+    public enum AdventureTier implements IItemTier {
+        ADVENTURE(4, 2000, 15.0F, 7.0F, 420, () -> {
+            return Ingredient.fromItems(ItemInit.basic_item);
+        });
+
+        private final int harvestLevel;
+        private final int maxUses;
+        private final float efficiency;
+        private final float attackDamage;
+        private final int enchantability;
+        private final LazyValue<Ingredient> repairMaterial;
+
+        private AdventureTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+            this.harvestLevel = harvestLevel;
+            this.maxUses = maxUses;
+            this.efficiency = efficiency;
+            this.attackDamage = attackDamage;
+            this.enchantability = enchantability;
+            this.repairMaterial = new LazyValue<>(repairMaterial);
+        }
+
+        @Override
+        public int getMaxUses() {
+            return this.maxUses;
+        }
+
+        @Override
+        public float getEfficiency() {
+            return this.efficiency;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return this.attackDamage;
+        }
+
+        @Override
+        public int getHarvestLevel() {
+            return this.harvestLevel;
+        }
+
+        @Override
+        public int getEnchantability() {
+            return this.enchantability;
+        }
+
+        @Override
+        public Ingredient getRepairMaterial() {
+            return this.repairMaterial.getValue();
+        }
+    }
 }
