6a8cc29c7ecd95ab245ae3cc6baed72536aa6e98
LewinGerber <lewin.gerber@stud.kbw.ch>
2020-06-18 20:37:33 +0200

    Code Structure

--- main/java/com/example/nawm/objects/items/BasicItem.java
+++ main/java/com/example/nawm/objects/items/BasicItem.java
@@ -1,13 +1,10 @@
 package com.example.nawm.objects.items;
 
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
